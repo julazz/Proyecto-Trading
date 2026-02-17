6d5fddfb8eb6707ca1129b377b33c81e8a3919ef
julazzzz <julas.alt@gmail.com>
2026-02-17 18:07:39 -0500

    Se añadio el proceso de crear .csv por cada stock

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import finnhub
 from datetime import datetime
+import pandas as pd
 client= finnhub.Client(api_key="d617g2pr01qjrruh84b0d617g2pr01qjrruh84bg")
 
-#print(datetime.fromtimestamp(client.quote("NVDA")["t"],tz=None))
-print(client.quote("NVDAFSADS"))
+
 
 while True:
     s=input("Ingrese el stock que desea visualizar: ").strip().upper()
+
     if client.quote(s)["c"]==0:
         print("El stock no existe")
+
     else:
-        print(client.quote(s))
+        data=client.quote(s)
+        
+        precio=data["c"]
+        cambio=data["d"]
+        porcentaje=data["dp"]
+        open=data["o"]
+        fecha=str(datetime.fromtimestamp(data["t"],tz=None))
+        dia,hora=fecha.split(" ")
+
+        df=pd.DataFrame({
+            "Fecha":[dia],
+            "Hora":[hora],
+            "Precio":[precio],
+            "Cambio":[cambio],
+            "Porcentaje":[porcentaje],
+            "Open":[open]
+            })
+        
+        file=f"{s}.csv"
+        df.to_csv(file,mode="a",index=False,header=not pd.io.common.file_exists(file))
